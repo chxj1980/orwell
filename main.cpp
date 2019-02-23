c3814daeeb3d870eb849340d66753c62a242dc72
Lucas Zanella <me@lucaszanella.com>
2019-02-23 22:14:51 +0000

    code cleanup

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ int main(int argc, char **argv)
 
     QQuickView view;
     view.setResizeMode(QQuickView::SizeRootObjectToView);
-    view.setSource(QUrl("qrc:/main2.qml"));
+    view.setSource(QUrl("qrc:/main.qml"));
     view.show();
 
     return app.exec();
