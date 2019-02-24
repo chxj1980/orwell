12e6916d3892298441388bfc57a261fb830a655f
Lucas Zanella <me@lucaszanella.com>
2019-02-24 01:42:06 -0300

    chowned objects

--- orwellJS/index.desktop.js
+++ orwellJS/index.desktop.js
@@ -32,7 +32,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#242633',
   },
   welcome: {
     fontSize: 20,
@@ -41,7 +41,7 @@ const styles = StyleSheet.create({
   },
   instructions: {
     textAlign: 'center',
-    color: '#333333',
+    color: '#FFFFFF',
     marginBottom: 5,
   },
 });
