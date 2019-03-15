160d4c9767cd0d00081fb50e6726eaa10a3294fc
Lucas Zanella <me@lucaszanella.com>
2019-03-14 23:49:33 -0300

    docker kinda works now

--- docker/run.sh
+++ docker/run.sh
@@ -3,4 +3,4 @@ USER_=$USER
 #Then it's just a matter of sending to github and rebuilding the docker with no cache.
 INSERT_LOCAL="-v /home/$USER_/python-rtsp-client:/home/deps/python-rtsp-client"
 INSERT_LOCAL=
-xhost local:root && docker run --rm -it -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/$USER_/orwell:/home/orwell $(echo $INSERT_LOCAL) -e DISPLAY=unix$DISPLAY orwell && xhost -
+xhost local:root && docker run --rm -it -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/$USER_/orwell:/home/dev/orwell $(echo $INSERT_LOCAL) -e DISPLAY=unix$DISPLAY orwell && xhost -
