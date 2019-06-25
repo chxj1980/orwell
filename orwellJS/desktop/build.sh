4ead33ceeba9696af2942763bcf70dad2d8bf12e
Lucas Zanella <me@lucaszanella.com>
2019-06-25 00:21:08 -0300

    fixed path of qt

--- orwellJS/desktop/build.sh
+++ orwellJS/desktop/build.sh
@@ -34,4 +34,5 @@ echo "build.sh desktop fonts: "$desktopFonts
 rm -rf CMakeFiles CMakeCache.txt cmake_install.cmake Makefile
 
 # Build project
-/home/lz/cmake-3.14.0-rc2-Linux-x86_64/bin/cmake -DCMAKE_PREFIX_PATH=/home/lz/Qt5.11.2/5.11.2/gcc_64 -DCMAKE_BUILD_TYPE=Debug -DEXTERNAL_MODULES_DIR="$ExternalModulesPaths" -DJS_BUNDLE_PATH="$JsBundlePath" -DDESKTOP_FONTS="$desktopFonts" . && make 
+#TODO: change these to not need to put locally installed things on the path
+/home/lz/cmake-3.14.5-Linux-x86_64/bin/cmake -DCMAKE_PREFIX_PATH=/home/lz/Qt2/5.11.2/gcc_64 -DCMAKE_BUILD_TYPE=Debug -DEXTERNAL_MODULES_DIR="$ExternalModulesPaths" -DJS_BUNDLE_PATH="$JsBundlePath" -DDESKTOP_FONTS="$desktopFonts" . && make 
