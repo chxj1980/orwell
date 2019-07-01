f879eb8624e0cb6aa1437546bde501920271a797
Lucas Zanella <me@lucaszanella.com>
2019-06-30 23:42:09 -0300

    enhanced

--- OpenGlMaterialShader.cpp
+++ OpenGlMaterialShader.cpp
@@ -32,124 +32,119 @@ struct State
 class Shader : public QSGSimpleMaterialShader<State>
 {
     QSG_DECLARE_SIMPLE_COMPARABLE_SHADER(Shader, State);
-public:
-    const char *vertexShader() const override {
-        return GET_STR(
-                    attribute vec4 vertexIn;
-                    attribute vec2 textureIn;
-                    varying vec2 textureOut;
-                    void main(void)
-                    {
-                        gl_Position = vertexIn;
-                        textureOut = textureIn;
-                    }
-            );
-    }
-
-    const char *fragmentShader() const override {
-        return GET_STR(
-                    varying vec2 textureOut;
-                    uniform sampler2D tex_y;
-                    uniform sampler2D tex_u;
-                    uniform sampler2D tex_v;
-                    void main(void)
-                    {
-                        vec3 yuv;
-                        vec3 rgb;
-                        yuv.x = texture2D(tex_y, textureOut).r;
-                        yuv.y = texture2D(tex_u, textureOut).r - 0.5;
-                        yuv.z = texture2D(tex_v, textureOut).r - 0.5;
-                        rgb = mat3(1.0, 1.0, 1.0,
-                            0.0, -0.39465, 2.03211,
-                            1.13983, -0.58060, 0.0) * yuv;
-                        gl_FragColor = vec4(rgb, 1.0);
-                    }
+    public:
+        const char *vertexShader() const override {
+            return GET_STR(
+                        attribute vec4 vertexIn;
+                        attribute vec2 textureIn;
+                        varying vec2 textureOut;
+                        void main(void)
+                        {
+                            gl_Position = vertexIn;
+                            textureOut = textureIn;
+                        }
                 );
-    }
-
-    QList<QByteArray> attributes() const override
-    {
-        //return QList<QByteArray>() << "vertexIn" << VERTEX_ATTRIBUTE;
-        return {QByteArrayLiteral("vertexIn"), QByteArrayLiteral("textureIn")};
-    }
-
-    void initialize()
-    {
-        if (!program()->isLinked()) {
-            return; //shader not linked, exit otherwise we crash, BUG: 336272
         }
 
-        QSGSimpleMaterialShader<State>::initialize(); //What is this?
-        glFuncs = QOpenGLContext::currentContext()->functions();
-        program()->bind();
+        const char *fragmentShader() const override {
+            return GET_STR(
+                        varying vec2 textureOut;
+                        uniform sampler2D tex_y;
+                        uniform sampler2D tex_u;
+                        uniform sampler2D tex_v;
+                        void main(void)
+                        {
+                            vec3 yuv;
+                            vec3 rgb;
+                            yuv.x = texture2D(tex_y, textureOut).r;
+                            yuv.y = texture2D(tex_u, textureOut).r - 0.5;
+                            yuv.z = texture2D(tex_v, textureOut).r - 0.5;
+                            rgb = mat3(1.0, 1.0, 1.0,
+                                0.0, -0.39465, 2.03211,
+                                1.13983, -0.58060, 0.0) * yuv;
+                            gl_FragColor = vec4(rgb, 1.0);
+                        }
+                    );
+        }
+
+        QList<QByteArray> attributes() const override
+        {
+            return {QByteArrayLiteral("vertexIn"), QByteArrayLiteral("textureIn")};
+        }
+
+        void initialize()
+        {
+            if (!program()->isLinked()) {
+                return; //shader not linked, exit otherwise we crash, BUG: 336272
+            }
+
+            QSGSimpleMaterialShader<State>::initialize();
+            glFuncs = QOpenGLContext::currentContext()->functions();
+            program()->bind();
+            
+            datas[0] = new unsigned char[frameWidth*frameHeight];	//Y
+            datas[1] = new unsigned char[frameWidth*frameHeight/4];	//U
+            datas[2] = new unsigned char[frameWidth*frameHeight/4]; //V
+
+            glFuncs->glGenTextures(3, texs);
+        }
+
+        void updateState(const State *state, const State *) override
+        {
+            //TODO: do verification of old state and new state here. Don't know why but do it, I think it has to do with performance
         
-        datas[0] = new unsigned char[frameWidth*frameHeight];	//Y
-        datas[1] = new unsigned char[frameWidth*frameHeight/4];	//U
-        datas[2] = new unsigned char[frameWidth*frameHeight/4]; //V
+            /*
+            glFuncs->glVertexAttribPointer(A_VER, 2, GL_FLOAT, 0, 0, ver);
+            glFuncs->glEnableVertexAttribArray(A_VER);
 
-        //unis[0] = program()->uniformLocation("tex_y");
-        //unis[1] = program()->uniformLocation("tex_u");
-        //unis[2] = program()->uniformLocation("tex_v");
+            glFuncs->glVertexAttribPointer(T_VER, 2, GL_FLOAT, 0, 0, tex);
+            glFuncs->glEnableVertexAttribArray(T_VER);
+            */
 
-        glFuncs->glGenTextures(3, texs);
-    }
+            //Y
+            glFuncs->glBindTexture(GL_TEXTURE_2D, texs[0]);
+            glFuncs->glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+            glFuncs->glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+            glFuncs->glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, frameWidth, frameHeight, 0, GL_RED, GL_UNSIGNED_BYTE, 0);
 
-    void updateState(const State *state, const State *) override
-    {
-        //TODO: do verification of old state and new state here. Don't know why but do it, I think it has to do with performance
-       
-        /*
-        glFuncs->glVertexAttribPointer(A_VER, 2, GL_FLOAT, 0, 0, ver);
-        glFuncs->glEnableVertexAttribArray(A_VER);
+            //U
+            glFuncs->glBindTexture(GL_TEXTURE_2D, texs[1]);
+            glFuncs->glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+            glFuncs->glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+            glFuncs->glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, frameWidth/2, frameHeight / 2, 0, GL_RED, GL_UNSIGNED_BYTE, 0);
 
-        glFuncs->glVertexAttribPointer(T_VER, 2, GL_FLOAT, 0, 0, tex);
-        glFuncs->glEnableVertexAttribArray(T_VER);
-        */
+            //V
+            glFuncs->glBindTexture(GL_TEXTURE_2D, texs[2]);
+            glFuncs->glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+            glFuncs->glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+            glFuncs->glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, frameWidth / 2, frameHeight / 2, 0, GL_RED, GL_UNSIGNED_BYTE, 0);
 
-        //Y
-        glFuncs->glBindTexture(GL_TEXTURE_2D, texs[0]);
-        glFuncs->glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glFuncs->glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glFuncs->glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, frameWidth, frameHeight, 0, GL_RED, GL_UNSIGNED_BYTE, 0);
+            glFuncs->glActiveTexture(GL_TEXTURE0);
+            glFuncs->glBindTexture(GL_TEXTURE_2D, texs[0]);
+            glFuncs->glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, frameWidth, frameHeight, GL_RED, GL_UNSIGNED_BYTE, datas[0]);
+            glFuncs->glUniform1i(unis[0], 0);
 
-        //U
-        glFuncs->glBindTexture(GL_TEXTURE_2D, texs[1]);
-        glFuncs->glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glFuncs->glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glFuncs->glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, frameWidth/2, frameHeight / 2, 0, GL_RED, GL_UNSIGNED_BYTE, 0);
+            glFuncs->glActiveTexture(GL_TEXTURE0+1);
+            glFuncs->glBindTexture(GL_TEXTURE_2D, texs[1]); 
+            glFuncs->glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, frameWidth/2, frameHeight / 2, GL_RED, GL_UNSIGNED_BYTE, datas[1]);
+            glFuncs->glUniform1i(unis[1],1);
 
-        //V
-        glFuncs->glBindTexture(GL_TEXTURE_2D, texs[2]);
-        glFuncs->glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glFuncs->glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glFuncs->glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, frameWidth / 2, frameHeight / 2, 0, GL_RED, GL_UNSIGNED_BYTE, 0);
+            glFuncs->glActiveTexture(GL_TEXTURE0+2);
+            glFuncs->glBindTexture(GL_TEXTURE_2D, texs[2]);
+            glFuncs->glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, frameWidth / 2, frameHeight / 2, GL_RED, GL_UNSIGNED_BYTE, datas[2]);
+            glFuncs->glUniform1i(unis[2], 2);
 
-        glFuncs->glActiveTexture(GL_TEXTURE0);
-        glFuncs->glBindTexture(GL_TEXTURE_2D, texs[0]);
-        glFuncs->glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, frameWidth, frameHeight, GL_RED, GL_UNSIGNED_BYTE, datas[0]);
-        glFuncs->glUniform1i(unis[0], 0);
+            glFuncs->glDrawArrays(GL_TRIANGLE_STRIP,0,4);
 
-        glFuncs->glActiveTexture(GL_TEXTURE0+1);
-        glFuncs->glBindTexture(GL_TEXTURE_2D, texs[1]); 
-        glFuncs->glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, frameWidth/2, frameHeight / 2, GL_RED, GL_UNSIGNED_BYTE, datas[1]);
-        glFuncs->glUniform1i(unis[1],1);
+            //release some things here?
+        }
 
-        glFuncs->glActiveTexture(GL_TEXTURE0+2);
-        glFuncs->glBindTexture(GL_TEXTURE_2D, texs[2]);
-        glFuncs->glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, frameWidth / 2, frameHeight / 2, GL_RED, GL_UNSIGNED_BYTE, datas[2]);
-        glFuncs->glUniform1i(unis[2], 2);
-
-        glFuncs->glDrawArrays(GL_TRIANGLE_STRIP,0,4);
-
-        //release some things here
-    }
-
-    void resolveUniforms() override
-    {        
-        unis[0] = program()->uniformLocation("tex_y");
-        unis[1] = program()->uniformLocation("tex_u");
-        unis[2] = program()->uniformLocation("tex_v");
-    }
+        void resolveUniforms() override
+        {        
+            unis[0] = program()->uniformLocation("tex_y");
+            unis[1] = program()->uniformLocation("tex_u");
+            unis[2] = program()->uniformLocation("tex_v");
+        }
 
 private:
     int id_color;
@@ -161,22 +156,24 @@ private:
     bool firstRender = true;
     int frameWidth = 1920;
     int frameHeight = 1080;
+    /*
+    //Do I need to use these things?
     static const GLfloat ver[] = {
         -1.0f,-1.0f,
         1.0f,-1.0f,
         -1.0f, 1.0f,
         1.0f, 1.0f
     };
-
     static const GLfloat tex[] = {
         0.0f, 1.0f,
         1.0f, 1.0f,
         0.0f, 0.0f,
         1.0f, 0.0f
     };
+    */
 };
 
-class Node : public QSGGeometryNode
+class Node: public QSGGeometryNode//,public FrameUpdater
 {
     public:
         Node(QSGTexture *source, QSGTexture *target):m_source(source),m_target(target)
@@ -185,6 +182,9 @@ class Node : public QSGGeometryNode
             setMaterial(material);
             setFlag(OwnsMaterial, true);
             //setProgress(1.0);
+
+            //stream->setFrameUpdater((FrameUpdater *) this);
+            //boost::thread mediaThread(&MediaStream::run, stream);
 
             /*
             QSGGeometry *g = new QSGGeometry(QSGGeometry::defaultAttributes_TexturedPoint2D(), 4);
@@ -208,12 +208,14 @@ class Node : public QSGGeometryNode
         }
 
     private:
+        QSGSimpleMaterial<Shader> *material;
         QScopedPointer<QSGTexture> m_source;
         QScopedPointer<QSGTexture> m_target;
+        //MediaStream* stream;
 }
 
 
-class Item : public QQuickItem
+class Item: public QQuickItem
 {
     Q_OBJECT
 
@@ -230,6 +232,7 @@ class Item : public QQuickItem
                 n = new Node();
 
             //QSGGeometry::updateTexturedRectGeometry(n->geometry(), boundingRect(), QRectF(0, 0, 1, 1));
+            //This is how we change things, because updatePaintNode is the safe place to do it 
             //static_cast<QSGSimpleMaterial<State>*>(n->material())->state()->color = m_color;
 
             n->markDirty(QSGNode::DirtyGeometry | QSGNode::DirtyMaterial);//what is this?
@@ -245,7 +248,7 @@ class Item : public QQuickItem
         void setUri(const QString &a) {
             uri = a.toStdString();
         }
-
+        /*
         void setColor(const QColor &color) {
             if (m_color != color) {
                 m_color = color;
@@ -257,11 +260,12 @@ class Item : public QQuickItem
         QColor color() const {
             return m_color;
         }
+        */
 
     signals:
-        void colorChanged();
+        //void colorChanged();
 
     private:
-        QColor m_color;
+        //QColor m_color;
         
 };
