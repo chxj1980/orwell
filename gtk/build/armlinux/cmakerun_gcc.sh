494ab75d7d24c663645b2bf550cdd62285e84060
Lucas Zanella <me@lucaszanella.com>
2019-08-28 21:56:52 -0300

    changes

--- gtk/build/armlinux/cmakerun_gcc.sh
+++ gtk/build/armlinux/cmakerun_gcc.sh
@@ -1,3 +1,3 @@
 rm -rf ../../CMakeCache.txt ../../CMakeFiles/ CMakeFiles/ CMakeCache.txt
-cmake   -DCMAKE_TOOLCHAIN_FILE=toolchain_x86_64.cmake\
+cmake   -DCMAKE_TOOLCHAIN_FILE=toolchain.cmake\
         ../..
